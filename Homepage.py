cb7093e2849a09c7f259deb6c45d5e392862e818
rmldn <108430560+rmldn@users.noreply.github.com>
2022-09-18 22:37:44 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -8,7 +8,7 @@ with open("style.css") as f:
 with st.container():
     st.subheader("")
     st.subheader("Rory Marrast, MSc, MRSC")
-    image = Image.open('/app/portfolio/rm.jpg')
+    image = Image.open('rm.jpg')
     st.image(image,width=250)
     st.write("Data Analysis, Data Visualisation, Data Cleansing")
 #    st.write("Using Python to solve your business challenges and generate digestable visualisations")
