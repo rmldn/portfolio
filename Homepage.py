bbb11b04df5724c2fea8613aeb71d6e84eb7fd6e
rmldn <108430560+rmldn@users.noreply.github.com>
2022-09-19 00:22:17 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
+#import plotly.express as px
+#import plotly.graph_objects as go
 import matplotlib
 from matplotlib import pyplot as plt
 matplotlib.rcParams['figure.figsize'] = [30,10]
